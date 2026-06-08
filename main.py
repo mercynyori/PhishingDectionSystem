abd1c1e40a035ea7170bd4271d3ccf8a8465e5bf
Mercy Nyori <mmwega13@gmail.com>
2026-06-08 15:47:10 +0200

    Converted the Email type values to 1 and 0

--- main.py
+++ main.py
@@ -16,7 +16,21 @@ print(df.shape)
 df_smaller = df.sample(n=300, random_state=42)
 #saving the new clean dataset
 df_smaller.to_csv("clean_emails_300.csv", index=False)
+
 #results
 print(df_smaller.head())
 print("Saved rows:", len(df_smaller))
+
+# checking the Email type values
+print(df["Email Type"].unique())
+
+#Converting the Email type values to 1 and 0
+df["Email Type"] = df["Email Type"].map({
+    "Safe Email" : 0,
+    "Phishing Email" : 1
+})
+
+#confirmation 
+print(df["Email Type"].unique())
+
 
